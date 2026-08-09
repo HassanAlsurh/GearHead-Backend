fbe8c81ee7ca0f052648a29ab3a321a42bbda141
Hasan Ali <hassanalsurh@gmail.com>
2026-08-09 09:28:13 +0300

    Created the vehicle and serviceRecord Schemas

--- models/vehicle.js
+++ models/vehicle.js
@@ -1,1 +1,80 @@
-// Vehicle model and schema here + imbedded ServiceRecords Schema * 
+const mongoose = require('mongoose');
+
+const currentYear = new Date().getFullYear();
+const maxModelYear = currentYear + 1;
+
+const serviceRecordSchema = new mongoose.Schema({
+    date: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    category: {
+        type: String,
+        required: true,
+        enum: ['Maintenance', 'Repair', 'Modification', 'Detailing', 'Other'],
+        default: 'Other',
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 500
+    },
+    cost: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    mileageAtService: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+}, { timestamps: true });
+
+const vehicleSchema = new mongoose.Schema({
+    year: {
+        type: Number,
+        required: true,
+        min: 1900,
+        max: maxModelYear
+    },
+    make: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true
+    },
+    model: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true
+    },
+    mileage: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    image: {
+        type: String,
+        default: 'default-car.jpg'
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    // sharedUsers: [{
+    // }], //if I have time I will be trying to add a way to invite other user to a certain 'Vehicle'
+    serviceRecords: [serviceRecordSchema],
+}, { timestamps: true });
+
+const Vehicle = mongoose.model('Vehicle', vehicleSchema);
+module.exports = Vehicle;

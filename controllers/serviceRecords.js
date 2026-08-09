cdb2c8fec98ca9789d329b98edac17605a05e482
Hasan Ali <hassanalsurh@gmail.com>
2026-08-09 09:44:33 +0300

    Added the Create route for serviceRecords

--- controllers/serviceRecords.js
+++ controllers/serviceRecords.js
@@ -1,1 +1,38 @@
 // Service Records controller
+
+const Vehicle = require('../models/vehicle')
+
+const create = async (req, res) => {
+    try {
+        const vehicle = await Vehicle.findById(req.params.vehicleId);
+
+        if (!vehicle) {
+            return res.status(404).json({ err: 'Vehicle not found' })
+        }
+
+        if (!vehicle.owner.equals(req.user._id)) {
+            return res.status(403).send("You're not authorized to add records to this vehicle!")
+        }
+
+        let toPush = {
+            date: req.body.date,
+            category: req.body.category,
+            description: req.body.description,
+            cost: req.body.cost,
+            mileageAtService: req.body.mileageAtService,
+            owner: req.user._id,
+        }
+
+        vehicle.serviceRecords.push(req.body)
+
+        await vehicle.save()
+
+        res.status(201).json(vehicle);
+    } catch (err) {
+        res.status(500).json({ err: err.message })
+    }
+}
+
+module.exports = {
+    create,
+}
--- server.js
+++ server.js
@@ -36,6 +36,8 @@ app.get('/vehicles/:vehicleId', verifyToken, vehicleCtrl.show)
 app.put('/vehicles/:vehicleId', verifyToken, vehicleCtrl.update)
 app.delete('/vehicles/:vehicleId', verifyToken, vehicleCtrl.deleteVehicle)
 
+app.post('/vehicles/:vehicleId/service-records', verifyToken, serviceCtrl.create)
+
 app.listen(PORT, () => {
   console.log(`The express app is ready on port ${PORT}! 😀`)
 })

95a22216ed07a2930115e57ac214d2ec20820dc6
Hasan Ali <hassanalsurh@gmail.com>
2026-08-12 09:43:52 +0300

    Adding invitedShow to show vehicles which are under garages the user is invited to

--- controllers/vehicles.js
+++ controllers/vehicles.js
@@ -90,6 +90,24 @@ const show = async (req, res) => {
   }
 };
 
+const invitedShow = async (req, res) => {
+  try {
+    const vehicle = await Vehicle.findById(req.params.vehicleId)
+
+    if (!vehicle) {
+      return res.status(404).json({ err: "Vehicle not found" });
+    }
+
+    if (!vehicle.owner._id.equals(req.user._id)) {
+      return res.status(403).send("You're not authorized to view this vehicle!");
+    }
+
+    res.status(200).json(vehicle);
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+};
+
 const update = async (req, res) => {
   try {
     const vehicle = await Vehicle.findById(req.params.vehicleId);
